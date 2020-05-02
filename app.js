51f572af9b4e52ea3d6dfd2df48f220ee5189d8b
alex <ayalex@yahoo.com>
2020-05-02 18:50:10 +0200

    prueba Openshift

---
+++ app.js
@@ -0,0 +1,4 @@
+//TODO: cambios en el comentario para probar los cambios en git
+//prueba
+//hola
+console.log("hello Word!\n")
---
+++ app2.js
@@ -0,0 +1,4 @@
+//otro fichero para comprobar el uso de Git 
+//he creado una nueva rama
+//he realizado un nuevo cambio
+console.log("esto es un mensaje de Git")
